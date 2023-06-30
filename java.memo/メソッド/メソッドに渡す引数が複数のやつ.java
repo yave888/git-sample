e4f25b25bb63ed4c35e1884bcd34623bee81bed9
yave888 <voyager8931@gmail.com>
2023-07-01 04:49:36 +0900

    メソッド

--- java.memo/メソッド/メソッドに渡す引数が複数のやつ.java
+++ java.memo/メソッド/メソッドに渡す引数が複数のやつ.java
@@ -1,1 +1,11 @@
-//値を複数渡す:メソッド名(値,値,...)
+public class Main{
+    public static void email(String title,String address,String text){
+        System.out.println("メール件名:" + title);
+        System.out.println("メールアドレス:" + address);
+        System.out.println("本文:" + text);
+    }
+    public static void main(String[] args){
+        System.out.println("メソッドを呼び出します");
+        email("カジュアル面談のお知らせ","voyager8931@gamil.com","7/4に予約しました。");
+    }
+}
